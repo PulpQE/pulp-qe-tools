dd2674df95dbc68c2961fa79e7bc7cd3faef4702
Bruno Rocha <rochacbruno@users.noreply.github.com>
2018-08-14 12:09:34 -0300

    Update reset_pulp.sh

--- pulp2/scripts/reset_pulp.sh
+++ pulp2/scripts/reset_pulp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# curl https://raw.githubusercontent.com/PulpQE/pulp-qe-tools/master/pulp2/scripts/reset_pulp.sh | bash
 systemctl stop httpd pulp_workers pulp_celerybeat pulp_resource_manager qpidd
 mongo pulp_database --eval 'db.dropDatabase()'
 runuser --shell /bin/sh apache --command pulp-manage-db
