cfcfad621e338c0c71419acf375761c05012142a
Bruno Rocha <rochacbruno@gmail.com>
2018-08-16 13:27:39 -0300

    ncluded some echoes

--- pulp2/scripts/setup_runner.sh
+++ pulp2/scripts/setup_runner.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Considering you have cloned both repos:
-# git clone https://github.com/pulpqe/pulp-smash
-# git clone https://github.com/pulpqe/pulp-2-tests
+# Clone the repos
+
+git clone https://github.com/pulpqe/pulp-smash
+git clone https://github.com/pulpqe/pulp-2-tests
+
+# Setup venv
 
 python3 -m venv venv 
 source venv/bin/activate
@@ -11,10 +14,17 @@ pip install pytest pytest-html
 pip install -e pulp-smash[dev]
 pip install -e pulp-2-tests[dev]
 
+# Check if smash is installed
 pulp-smash settings save-path
 
 # Set your settings for the target system and then
-# export PULP_SMASH_CONFIG_FILE=path_to_a_file.json
+echo "Set smash settings location"
+echo "export PULP_SMASH_CONFIG_FILE=path_to_a_file.json"
+
+# create settings file
+echo "Create your settings file"
+echo "pulp-smash settings create"
 
 # Run test runner
-# py.test -v --color=yes --html=test_report.html --self-contained-html --pyargs pulp_2_tests.tests
+echo "Run tests and save html report"
+echo "py.test -v --color=yes --html=test_report.html --self-contained-html --pyargs pulp_2_tests.tests"
