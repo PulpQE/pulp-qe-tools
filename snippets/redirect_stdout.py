4894790fbe52ae76c002f0190dc03395b49282af
Bruno Rocha <rochacbruno@users.noreply.github.com>
2018-08-17 14:17:47 -0300

    Update redirect_stdout.py

--- snippets/redirect_stdout.py
+++ snippets/redirect_stdout.py
@@ -4,7 +4,7 @@
 """
 In python there are some functions
 which `prints` its output to stdout/stderr
-instead of retruning the value.
+instead of returning the value.
 """
 
 # example
@@ -19,15 +19,15 @@ help(int)  # <- prints out the docstring of `int` to stdout
 Problem: We want to capture and store the value of those functions
 """
 
-# wring attemp
+# wrong attemp
 
-help_of_int = help(int)  # <-- Will be None as the help does not retuns
+help_of_int = help(int)  # <-- Will be None as the `help` function does not returns any value
 
 """
 Solution: Redirect the stdout output to a file like object
 """
 
-import contextlib, io
+import contextlib
 
 with open('output.txt', 'w') as f:  #  can use io.BytesIO in place of file
     with contextlib.redirect_stderr(f):
@@ -40,4 +40,6 @@ my_output = open('output.txt').read()
 print(my_output)
 
 # caveats: Each call inside context manager will overwrite the values of the file
+#          So only the last call is saves, solution is to use `a` or place 
+#          calls inside different `with` statements and files.
 
